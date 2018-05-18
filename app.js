bca9101b6b5ae20191f2f97cd5a29e8a5a108f0c
hugo.pievic <hugo.pievic@manomano.com>
2018-05-18 10:01:08 +0200

    Added formidable package

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const app = express();
-const swaggerUi = require('swagger-ui-express');
+const formidable = require('express-formidable');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-
+const swaggerUi = require('swagger-ui-express');
 const usersStore = require('./users');
 
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const API_PORT = 3000;
+
+const app = express();
+
+app.use(formidable());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 console.log(
-  `Node app running on port ${API_PORT} (http://localhost:${API_PORT}`
+  `Node app running on port ${API_PORT} & Doc available at http://localhost:${API_PORT}/api-docs`
 );
-console.log(`Doc available at http://localhost:${API_PORT}/api-docs`);
 app.listen(API_PORT);
